b0f2a54118f5667c5d225535ea95e0c0dfa518d3
Khulanbq <khulanb77@gmail.com>
2022-01-15 11:48:13 +0800

    work1

---
+++ work1.py
@@ -0,0 +1,30 @@
+# Sample code
+# Ctrl + Shift + P
+print("Test")
+print('Testing 2') # it is an example
+a = 15
+print('a is: ', a)
+
+
+# it is also an example
+print('Lower in format a is: {}'.format(a))
+
+# Basic operators
+1+2 # add
+5-9 # subtract
+5*5 # multiply
+5/8 # divide
+5**2 # power ^
+5**(1/2) # power
+
+# boolean
+my_condition0 = True
+my_condition1 = False
+
+print(my_condition1)
+
+# string variable
+c = 'hello'
+
+# m = input("Enter a number: ")
+# print("You entered: " + str(m) + '. Done!')
